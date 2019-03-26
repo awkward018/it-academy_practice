048c06783184db3fcb6e896f1276c67dce4bcfde
саня высочина <2001sashka@tut.by>
2019-03-26 20:00:20 +0300

    commit

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[13];
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next();
+                Console.WriteLine(array[i]);
+                    }
+
+            Console.WriteLine("max Value:");
+            int maxValue = array.Max();
+            Console.WriteLine(maxValue);
+
+            Console.ReadLine();
+            
+        }
+    }
+}
